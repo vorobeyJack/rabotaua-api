b80a0b31ee39ae4e0f6a8a1bb1f4ac72331c066c
Evgenii Vorobey <evgeniivorobey1404@gmail.com>
2018-04-08 22:02:03 +0300

    Added Exception class/ResourceContainer upds

--- src/Http/ResourceContainer.php
+++ src/Http/ResourceContainer.php
@@ -2,5 +2,48 @@
 
 namespace vrba\rabotaApi\Http;
 
+use vrba\rabotaApi\Exception\UndefinedResourceException;
+use vrba\rabotaApi\Resource\Resource;
+
 class ResourceContainer
-{}
+{
+    /**
+     * @var array $endpoints
+     */
+    private $endpoints = [];
+
+    /**
+     * Add resource if not exists.
+     *
+     * @param string $endpoint
+     * @return ResourceContainer
+     * @throws UndefinedResourceException
+     */
+    private function addResourceEndpoint(string $endpoint) : ResourceContainer
+    {
+        $class = __NAMESPACE__ . '\\' . ucfirst($endpoint);
+
+        if (!$this->isResourceExists($endpoint)) {
+            throw new UndefinedResourceException();
+        }
+
+        $this->endpoints[$endpoint] = new $class($this);
+
+        return $this;
+    }
+
+    /**
+     * Check is resource exists.
+     *
+     * @param $class
+     * @return bool
+     */
+    private function isResourceExists($class) : bool
+    {
+        if (class_exists($class)) {
+            return false;
+        }
+
+        return is_subclass_of(new $class, Resource::class);
+    }
+}
