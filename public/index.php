3e67f5accbd3bd58735ac5d941bab1d626a035ab
Evgenii Vorobey <evgeniivorobey1404@gmail.com>
2018-04-28 23:28:12 +0300

    master Account entitty upds

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ use vrba\rabotaApi\Service;
 $api = Service::run();
 
 try {
-    dump($api->account->getPhoto());
+    dump($api->resume->createWithPersonalData(702, []));
 } catch (\Throwable $e) {
     dump(new \vrba\rabotaApi\Http\Response\ErrorResponse($e));
     die;
--- src/Resource/Resume.php
+++ src/Resource/Resume.php
@@ -18,6 +18,7 @@ class Resume extends Resource
      * Sub resource.
      */
     public const STATE = 'state';
+    public const PERSONAL_INFO = 'personal';
 
     /**
      * Returns resume of auth user.
@@ -48,6 +49,32 @@ class Resume extends Resource
      */
     public function getState(int $id)
     {
-        return $this->execute('GET', $id . '/'. self::STATE);
+        return $this->execute('GET', $id . '/' . self::STATE);
+    }
+
+    /**
+     * Creates resume with user's personal data, personalData:
+     * All fields are required
+     * {
+     *  "name": "string",
+     *  "middleName" : "string",
+     *  "surName" : "string",
+     *  "dateBirth": "2018-04-23T15:50:30.736Z",
+     *  "gender": 0
+     *  "cityId": 0,
+     *  "moving": [
+     *       0
+     *  ],
+     *
+     *  "resumeId": 0
+     * }
+     *
+     * @param int $resumeId
+     * @param array $personalData
+     * @return mixed
+     */
+    public function createWithPersonalData(int $resumeId, array $personalData = [])
+    {
+        return $this->execute('POST', $resumeId . '/' . self::PERSONAL_INFO, $personalData);
     }
 }
